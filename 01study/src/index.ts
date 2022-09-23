10d3f40b807793fd4107170a25ebedc3046757a3
ttom921 <ttom921@hotmail.com>
2022-09-23 14:40:14 +0800

    Graphics

--- 01study/src/index.ts
+++ 01study/src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Container, Sprite } from "pixi.js";
+import { Application, Container, Graphics, Point, Sprite } from "pixi.js";
 
 const app = new Application({
     view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -9,12 +9,32 @@ const app = new Application({
     height: 480,
 });
 
-const conty: Container = new Container();
-conty.x = 200;
-conty.y = 0;
-app.stage.addChild(conty);
+const graphy: Graphics = new Graphics();
 
-const clampy: Sprite = Sprite.from("clampy.png");
-clampy.x = 100;
-clampy.y = 100;
-conty.addChild(clampy);
+// we give instructions in order. begin fill, line style, draw circle, end filling
+graphy.beginFill(0xFF00FF);
+graphy.lineStyle(10, 0x00FF00);
+graphy.drawCircle(0, 0, 25); // See how I set the drawing at 0,0? NOT AT 100, 100!
+graphy.endFill();
+
+app.stage.addChild(graphy);
+
+// Here we set it at 100,100
+graphy.x = 100;
+graphy.y = 100;
+
+const gRect: Graphics = new Graphics();
+gRect.beginFill(0xFF00FF);
+gRect.lineStyle(10, 0x00FF00);
+gRect.drawRect(0, 0, 25, 25); // See how I set the drawing at 0,0? NOT AT 100, 100!
+gRect.endFill();
+app.stage.addChild(gRect);
+
+
+gRect.x = 130;
+gRect.y = 130;
+
+
+
+
+
