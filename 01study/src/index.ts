d3336abb48f10c0dca8162061ec3ff15744305df
ttom921 <ttom921@hotmail.com>
2022-09-23 16:47:02 +0800

    Particles

--- 01study/src/index.ts
+++ 01study/src/index.ts
@@ -1,6 +1,7 @@
-import { BlurFilter } from "@pixi/filter-blur";
-import { Application, Container, Graphics, Point, Sprite, TextStyle, Text, BitmapText, BitmapFont } from "pixi.js";
-
+//import { BlurFilter } from "@pixi/filter-blur";
+import { Application, Container, Graphics, Point, Sprite, TextStyle, Text, BitmapText, BitmapFont, ParticleContainer, Texture } from "pixi.js";
+import * as particleSettings from "./emitter.json";
+import * as particles from '@pixi/particle-emitter';
 const app = new Application({
     view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
     resolution: window.devicePixelRatio || 1,
@@ -10,20 +11,26 @@ const app = new Application({
     height: 480,
 });
 
-const clampy: Sprite = Sprite.from("clampy.png");
+// const clampy: Sprite = Sprite.from("clampy.png");
 
-clampy.anchor.set(0.5);
+// clampy.anchor.set(0.5);
 
-// setting it to "the middle of the screen
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
+// // setting it to "the middle of the screen
+// clampy.x = app.screen.width / 2;
+// clampy.y = app.screen.height / 2;
 
-app.stage.addChild(clampy);
+// app.stage.addChild(clampy);
 
-// Make your filter
-const myBlurFilter = new BlurFilter();
+const particleContainer = new ParticleContainer();
+app.stage.addChild(particleContainer);
 
-// Add it to the `.filters` array of any DisplayObject
-clampy.filters = [myBlurFilter];
+const emitter = new particles.Emitter(particleContainer,
+    particles.upgradeConfig(particleSettings, [Texture.from("particle.png")])
+);
+emitter.autoUpdate = true; // If you keep it false, you have to update your particles yourself.
+emitter.updateSpawnPos(200, 100);
+emitter.emit = true;
+
+
 
 
