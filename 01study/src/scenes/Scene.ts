8f4b6a9aa0db7c504d65a8069560079553f066a8
ttom921 <ttom921@hotmail.com>
2022-09-27 16:00:07 +0800

    Ticker

--- 01study/src/scenes/Scene.ts
+++ 01study/src/scenes/Scene.ts
@@ -1,4 +1,4 @@
-import { Container, Sprite } from "pixi.js";
+import { Container, Sprite, Ticker } from "pixi.js";
 
 
 export class Scene extends Container {
@@ -6,6 +6,7 @@ export class Scene extends Container {
     private readonly screenHeight: number;
     // we promoted clampy to a member of the class
     private clampy: Sprite;
+    private clampyVelocity: number = 5;
     constructor(screenWidth: number, screenHeight: number) {
         super();//Mandatoyr! This calls the superclass constructor.
         //see how members of the class need `this.`?
@@ -16,8 +17,21 @@ export class Scene extends Container {
         this.clampy = Sprite.from("clampy.png");
 
         this.clampy.anchor.set(0.5);
-        this.clampy.x = this.screenWidth / 2;
+        this.clampy.x = 0;//we start it at 0
         this.clampy.y = this.screenHeight / 2;
         this.addChild(this.clampy);
+
+        //see this `this` thingy there? That is another way of binding the context!
+        Ticker.shared.add(this.update, this);
+        //If you want,you cna do ite the bind way
+        //Tick.shared.add(this.update.bind(this));
+
+    }
+    private update(deltaTime: number): void {
+        this.clampy.x = this.clampy.x + this.clampyVelocity * deltaTime;
+        if (this.clampy.x > this.screenWidth) {
+            // Woah there clampy, com back inside the screen!
+            this.clampy.x = 0;
+        }
     }
 }
